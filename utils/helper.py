52bdeb57cdf427b9073b799573f34bc844675e4e
Sumit Binnani <sumit.binnani@gmail.com>
2017-02-24 21:46:36 +0530

    Update Pipeline

--- utils/helper.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-
-
-def show_image_cv2(img):
-    """Returns cv2 image
-    in matplotlib format
-    """
-    return plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
--- utils/thresholding.py
+++ utils/thresholding.py
@@ -1,36 +1,73 @@
 import cv2
 import numpy as np
 
+def dir_threshold(img, sobel_kernel=15, thresh=(0.7, 1.2)):
+    # Grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray[:,:,2], cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray[:,:,2], cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction, 
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    binary_output =  np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
 
-def s_threshold(img, min=170, max=255):
-    """Returns S channel thresholded
-    by min and max values
-    """
-    # Convert to HLS color space and separate the S channel
-    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-    s_channel = hls[:, :, 2]
-    s_binary = np.zeros_like(s_channel)
-    s_binary[(s_channel >= min) & (s_channel <= max)] = 1
-    return s_binary
+    # Return the binary image
+    return binary_output
+
+# Define a function to return the magnitude of the gradient
+# for a given sobel kernel size and threshold values
+def mag_thresh(img, sobel_kernel=9, mag_thresh=(30, 255)):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray[:,:,2], cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray[:,:,2], cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255 
+    gradmag = (gradmag/scale_factor).astype(np.uint8) 
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+
+    # Return the binary image
+    return binary_output
 
 
-def sobelx_threshold(gray, min=20, max=255):
-    """Returns sobel derviative in x-direction thresholded
-    by min and max values
-    """
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)  # Take the derivative in x
-    abs_sobelx = np.absolute(sobelx)  # Absolute x derivative to accentuate lines away from horizontal
-    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
-    sxbinary = np.zeros_like(scaled_sobel)
-    sxbinary[(scaled_sobel >= min) & (scaled_sobel <= max)] = 1
-    return sxbinary
-
-
-def hsv_mask(image):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_white = np.array([0,0,225], dtype=np.uint8)
-    upper_white = np.array([180,255,255], dtype=np.uint8)
-    mask = cv2.inRange(hsv, lower_white, upper_white)
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-
+def threshold(img, color=False, mag_dir_thresh=False):
+    """Threshhold image on saturation channel and 
+    using magnitude gradient"""
+    img = np.copy(img)
+    
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    
+    
+    ## White Color
+    lower_white = np.array([0,210,0], dtype=np.uint8)
+    upper_white = np.array([255,255,255], dtype=np.uint8)
+    white_mask = cv2.inRange(hls, lower_white, upper_white)
+    
+    ## Yellow Color
+    lower_yellow = np.array([18,0,100], dtype=np.uint8)
+    upper_yellow = np.array([30,220,255], dtype=np.uint8)
+    yellow_mask = cv2.inRange(hls, lower_yellow, upper_yellow)  
+    
+    combined_binary = np.zeros_like(white_mask)
+    
+    # Dir Mag Threshold
+    if mag_dir_thresh:
+        dir_mask = dir_threshold(img)
+        mag_mask = mag_thresh(img)
+        combined_binary[((dir_mask == 1) & (mag_mask == 1))] = 255
+        
+    if color:
+        return np.dstack((white_mask, yellow_mask, combined_binary))
+    
+    else:
+        combined_binary[((white_mask == 255) | (yellow_mask == 255))] = 255
+        combined_binary[(combined_binary == 255)] = 1
+        return combined_binary
